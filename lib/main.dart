c0a1f44739dbfa780e3b0ac0b6925d5ee4402e59
Rokongrelowiec <juliusz99@gmail.com>
2022-11-04 14:01:05 +0100

    Simplify code and add theme choice

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:studentifier/models/theme_provider.dart';
 
 import './screens/home_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => ThemeProvider()..initialize(),
+          ),
+        ],
+        child: const MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Studentifier',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: HomeScreen(),
+    return Consumer<ThemeProvider>(
+      builder: (context, provider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Studentifier',
+          themeMode: provider.themeMode,
+          theme: ThemeData(
+            primarySwatch: Colors.orange,
+            navigationBarTheme: NavigationBarThemeData(
+              backgroundColor: Colors.orange[300],
+              indicatorColor: Colors.orangeAccent[400],
+            ),
+            textTheme: TextTheme(
+              headline1: TextStyle(color: Colors.black),
+            ),
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
+          darkTheme: ThemeData(
+            primarySwatch: Colors.orange,
+            scaffoldBackgroundColor: Colors.grey[900],
+            navigationBarTheme: NavigationBarThemeData(
+                backgroundColor: Colors.black, indicatorColor: Colors.white30),
+            drawerTheme: DrawerThemeData(backgroundColor: Colors.grey[850]),
+            textTheme: TextTheme(
+              headline1: TextStyle(color: Colors.white),
+            ),
+            iconTheme: IconThemeData(color: Colors.white),
+          ),
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
