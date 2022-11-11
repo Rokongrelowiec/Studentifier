73451898d50f44b1c85b2945cf6cd02a1244fc4f
Rokongrelowiec <juliusz99@gmail.com>
2022-11-11 11:38:17 +0100

    Add custom error widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:studentifier/screens/login_screen.dart';
 
 import './models/theme_provider.dart';
 import './screens/animated_splash_screen.dart';
-
 import './screens/home_screen.dart';
+import './screens/login_screen.dart';
 
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (_) => ThemeProvider()..initialize(),
-          ),
-        ],
-        child: const MyApp(),
+void main() {
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    bool inDebug = false;
+    assert(() {
+      inDebug = true;
+      return true;
+    }());
+    if (inDebug) return ErrorWidget(details.exception);
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Error!\n${details.exception}',
+        style: const TextStyle(
+            color: Colors.orangeAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 20),
+        textAlign: TextAlign.center,
       ),
     );
+  };
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider()..initialize(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/screens/animated_splash_screen.dart
+++ lib/screens/animated_splash_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart' as ASP;
+import 'package:animated_splash_screen/animated_splash_screen.dart' as ASS;
 import 'package:page_transition/page_transition.dart';
 
 import './login_screen.dart';
@@ -21,35 +21,36 @@ class GenerateAnimatedSplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Color(0xFFE0664A), Colors.orange]),
-        ),
-        child: ASP.AnimatedSplashScreen(
-          backgroundColor: Colors.transparent,
-          splashIconSize: 250,
-          splash: Column(
-            children: [
-              Icon(
-                Icons.layers,
-                color: Colors.white,
-                size: 90,
-              ),
-              Text("Studentifier", style: TextStyle(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.deepOrangeAccent, Colors.orange]),
+      ),
+      child: ASS.AnimatedSplashScreen(
+        backgroundColor: Colors.transparent,
+        splashIconSize: 250,
+        splash: Column(
+          children: [
+            Icon(
+              Icons.layers,
+              color: Colors.white,
+              size: 90,
+            ),
+            Text(
+              "Studentifier",
+              style: TextStyle(
                   color: Colors.white,
                   fontSize: 40,
-                  fontWeight: FontWeight.bold
-              ),)
-            ],
-          ),
-          nextScreen: LoginScreen(),
-          duration: 1500,
-          splashTransition: ASP.SplashTransition.sizeTransition,
-          pageTransitionType: PageTransitionType.rightToLeft,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
         ),
-
+        nextScreen: LoginScreen(),
+        duration: 1500,
+        splashTransition: ASS.SplashTransition.sizeTransition,
+        pageTransitionType: PageTransitionType.rightToLeft,
+      ),
     );
   }
 }
